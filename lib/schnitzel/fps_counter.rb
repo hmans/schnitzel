0bc2849421da0a7ffde648fa354feb8842dab841
Hendrik Mans <hendrik@mans.de>
2013-06-17 17:43:18 +0200

    Simplify FPS counter :)

--- lib/schnitzel/fps_counter.rb
+++ lib/schnitzel/fps_counter.rb
@@ -1,27 +1,13 @@
 module Schnitzel
   class FpsCounter < Node
-    def initialize(interval: 250, **args)
+    def initialize(**args)
       super(args)
-      @interval = interval
-      @interval_seconds = @interval / 1000.0
-      @frames = 0
-      @nexttick = 0
-
       @text = Text.new(position: [10, 456], color: 0xccddffdd, size: 14)
       self << @text
     end
 
     def update(dt)
-      # Increment frame counter
-      @frames += 1
-
-      # If a second has passed, update text.
-      if Gosu::milliseconds >= @nexttick
-        @nexttick += @interval
-        fps = @frames / @interval_seconds
-        @text.text = "FPS: #{fps}"
-        @frames = 0
-      end
+      @text.text = "FPS: #{Gosu::fps}"
     end
   end
 end
